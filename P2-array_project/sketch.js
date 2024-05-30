9b8177f25ba266c14a86d22778f71f4362b02b45
Consumerofbread56 <96402371+Consumerofbread56@users.noreply.github.com>
2024-05-30 12:16:02 -0600

    p2
    worked on stuff

--- P2-array_project/sketch.js
+++ P2-array_project/sketch.js
@@ -5,23 +5,39 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let beat;
+const SPEED = 5;
+targets = []
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  beat = millis();
 }
 
 function draw() {
   background(220);
+  spawnCircles();
+  drawCircle();
 }
 
 function drawCircle(){
-
+  circle(targets[0].x, targets[0].y, targets[0].radius);
 }
 
 function spawnCircles(){
-  someCircle={
-    
-    
+  
+  someCircle = {
+    x: random(width),
+    y: random(height),
+    speed: SPEED,
+    radius: 10
   }
+  if (millis()-1000 === beat){
+    targets.pop(someCircle);
+    targets.push(someCircle);
+    beat = millis();
+  }
+  
 }
   
